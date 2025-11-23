f57212cc01f8796205c16e2bfe4c5740b83e3f9e
Juan José <j_jose8888@hotmail.com>
2025-11-23 19:01:02 +0100

    Creada la calculadora de comisiones

---
+++ mis_comisiones.py
@@ -0,0 +1,11 @@
+print("Bienvenido a la calculadora de comisiones.")
+print("------------------------------------------")
+
+nombre = input("Dime tu nombre: ")
+ventas = int(input("Dime el valor de tus ventas en $ de este mes: "))
+
+valor_comision = round(ventas * (13 / 100), 2)
+
+valor_total = ventas + valor_comision
+
+print(f"Hola {nombre}. Con el valor de ventas de {ventas}$ te corresponde, con una comisión del 13%, una comision de {valor_comision}$ y un ingreso total de {valor_total}$")
